22f7e2d8f27760c1a1ab75c39b8fcbf1f14d4477
Muhammad-Yusuf-2766 <strangerbellimo@gmail.com>
2026-02-04 09:51:42 +0900

    socket-url fixed

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,7 +1,9 @@
-// src/socket.ts
 import { io, Socket } from 'socket.io-client'
 
-const socket: Socket = io(`${import.meta.env.VITE_SERVER_BASE_SOCKET_URL}`, {
+const apiBase = import.meta.env.VITE_SERVER_BASE_URL as string
+const socketBase = apiBase.replace(/\/api\/?$/, '') // oxiridagi /api yoki /api/ ni olib tashlaydi
+
+const socket: Socket = io(socketBase, {
 	transports: ['websocket'],
 	autoConnect: true,
 })
